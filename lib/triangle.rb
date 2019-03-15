f2ae6b4979f20914e05c858d869cf9250e06cd58
Mat Anderson <matandersonlv@hotmail.com>
2019-03-15 23:47:05 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,20 +1,7 @@
 class Triangle
-  attr_accessor :a, :b, :c
-  
-    def initialize(a, b, c)
-      if a <= 0 || b <= 0 || c <= 0 || a + b <= c || a + c <= b || b + c <= a 
-        raise TriangleError
-        
-      else
-        @a = a 
-        @b = b 
-        @c = c 
-      end
-      
-    end
-  
-  class TriangleError < StandardError 
-    
+  # triangle code
+ 
+  class TriangleError < StandardError
+    # triangle error code
   end
-  
 end
