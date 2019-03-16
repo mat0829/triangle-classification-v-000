12849fb1a245b03801e14e917538f6fd90d73019
Mat Anderson <matandersonlv@hotmail.com>
2019-03-16 00:38:47 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,7 @@ class Triangle
   
   if (side_a + side_b) <= side_c || (side_a + side_c) <= side_b || (side_b + side_c) <= side_a || (side_a * side_b * side_c) == 0
     
-    begin 
+  begin 
       raise TriangleError
       
   elsif side_a == side_b && side_c
