c432498c1027f9a09950a7f798a08924ee64123a
Mat Anderson <matandersonlv@hotmail.com>
2019-03-16 00:12:15 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,14 +1,14 @@
 class Triangle
   # triangle code
- 
-  class TriangleError < StandardError
-    # triangle error code
-    
-    def initialize(:side_a, :side_b, :side_c)
+  
+  def initialize(:side_a, :side_b, :side_c)
       @side_a = side_a
       @side_b = side_b 
       @side_c = side_c
     end
-    
+ 
+  class TriangleError < StandardError
+    # triangle error code
   end
+  
 end
