3520be733c559507c27f17a11d8245ad362ca792
Mat Anderson <matandersonlv@hotmail.com>
2019-03-16 00:41:23 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,15 +16,22 @@ class Triangle
   
   def kind
     
-    if (side_a + side_b) <= side_c || (side_a + side_c) <= side_b || (side_b + side_c) <= side_a || (side_a * side_b * side_c) == 0
+    if (side_a + side_b) <= side_c || (side_a + side_c) <= side_b || (side_b + side_c) <= side_a ||
+      (side_a * side_b * side_c) == 0
     
-  begin 
+    begin 
       raise TriangleError
       
-  elsif side_a == side_b && side_c
-    self.kind = :equalateral
+    elsif side_a == side_b && side_c
+      self.kind = :equalateral
+      
+    elsif side_a == side_b && side_c
+      self.kind = :equalateral
+      
+    elsif side_a == side_b && side_c
+      self.kind = :equalateral
     
-  end
+    end
   end
   
 end
