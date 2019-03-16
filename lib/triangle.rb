557eb71c91fd95bbd3a255487c7405413a0b7b7f
Mat Anderson <matandersonlv@hotmail.com>
2019-03-16 00:34:18 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ class Triangle
     # triangle error code
   end
   
-  if (@side_a + @side_b) <= @side_c || (@side_a + @side_c) <= @side_b || (@side_b + @side_c) <= @side_a
+  if (@side_a + @side_b) <= @side_c || (@side_a + @side_c) <= @side_b || (@side_b + @side_c) <= @side_a || (@side_a * @side_b * @side_c) == 0
     raise TriangleError
   def kind
     
