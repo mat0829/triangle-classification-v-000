f9be73fcd53f1530644f355258a8f28d27e995a1
Mat Anderson <matandersonlv@hotmail.com>
2019-03-16 00:05:55 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,18 +1,1 @@
-class Triangle
-  # triangle code
-  attr_accessor :a, :b, :c 
-  
-  def initialize(a, b, c)
-    @a = a 
-    @b = b 
-    @c = c
-  end
-  
-  def kind
-    if @a + @b >= @c &&
-  end
- 
-  class TriangleError < StandardError
-    # triangle error code
-  end
-end
+
